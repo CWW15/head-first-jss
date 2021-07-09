b21906be196229dd1c1910be2e055b4b31283b9f
CWW15 <cwwalker1506@yahoo.com>
2021-07-09 13:12:23 +1000

    1 file modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-function startGame() {
-  myGamePiece = new component(30, 30, "red", 10, 120);
-  myGamePiece.gravity = 0.05;
-  myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-  myGameArea.start();
-}
+<html>
+<head>
+  <title>iRock - The Virtual Pet Rock </title>
+</head>
 
-var myGameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.frameNo = 0;
-  },
-  clear : function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-} 
+<body>
+  <div style="margin-top:100px; text-align:center">
+    <img id="rockImg" src="rock.png" alt="iRock" />
+  </div>
+</body>
+</html>
